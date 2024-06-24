bfe37f08cb63b6afc893c12f73a9bff09408c885
fraser.macinnes <fraser.macinnes@researchspace.com>
2024-06-24 16:29:12 +0100

    add dummy test back in as jenkins build relies on a test report being generated.

---
+++ src/test/java/com.researchspace.googleauth/GoogleTokenVerifierTest.java
@@ -0,0 +1,11 @@
+package com.researchspace.googleauth;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+public class GoogleTokenVerifierTest {
+  @Test
+  public void test() {
+    assertEquals(1,1);
+  }
+}
